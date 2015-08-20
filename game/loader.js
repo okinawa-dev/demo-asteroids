62c134752e67f790abebfdcab70a9cc7b051c298
neverbot <neverbot@neverbot.com>
2015-08-20 18:42:01 +0200

    Demo working, build with "npm run build"

--- game/loader.js
+++ game/loader.js
@@ -9,31 +9,31 @@ Game.Loader.prototype.initialize = function()
   // engine.preloader.addAnimation(anim_name, path, 
   // xStart, yStart, width, height, frames, initFrame, speed)
 
-  engine.preloader.addSprite({ name: 'starship', path: 'game/images/ship.png', width: 50, height: 50 });
+  engine.preloader.addSprite({ name: 'starship', path: 'images/ship.png', width: 50, height: 50 });
 
   // **************
   //  Enemies 
   // **************
-  engine.preloader.addSprite({ name: 'meteor', path: 'game/images/meteor.png', width: 60, height: 60 });
+  engine.preloader.addSprite({ name: 'meteor', path: 'images/meteor.png', width: 60, height: 60 });
 
   // **************
   //  Shots
   // **************
-  engine.preloader.addSprite({ name: 'shot', path: 'game/images/shot.png', width: 10, height: 10 });
+  engine.preloader.addSprite({ name: 'shot', path: 'images/shot.png', width: 10, height: 10 });
 
   // **************
   //  Effects
   // **************
-  engine.preloader.addAnimation({ name: 'explosion', path: 'game/images/effect.explosion.png', 
+  engine.preloader.addAnimation({ name: 'explosion', path: 'images/effect.explosion.png', 
                            xStart: 0, yStart: 0, width: 48, height: 47, frames: 11, initFrame: 0, speed: 14 });
-  engine.preloader.addSprite({ name: 'halo', path: 'game/images/effect.halo.png', width: 63, height: 63 });
+  engine.preloader.addSprite({ name: 'halo', path: 'images/effect.halo.png', width: 63, height: 63 });
 
 
   // **************
   //  Sounds
   // **************
-  engine.preloader.addSound(({ name: 'explosion', path: 'game/sounds/fridobeck_explosion.mp3' }));
-  engine.preloader.addSound(({ name: 'shot', path: 'game/sounds/halgrimm_shot.mp3' }));
+  engine.preloader.addSound(({ name: 'explosion', path: 'sounds/fridobeck_explosion.mp3' }));
+  engine.preloader.addSound(({ name: 'shot', path: 'sounds/halgrimm_shot.mp3' }));
 
   // First screen: preloader with progress bar
   engine.preloader.addBackground(engine.game.commonBackground);
