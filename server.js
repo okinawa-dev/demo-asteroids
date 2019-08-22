7c69238471c256a894e11ea2bfe472e1b00bf7ee
Iván Alonso <ivan@neverbot.com>
2019-08-22 17:55:20 +0200

    show url in console to allow click from terminal and auto opening in browser

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ var serveStatic = require('serve-static');
 connect()
   .use(serveStatic(__dirname))
   .listen(8000, function() {
-    console.log('Server running on 8000...');
+    console.log('Server running on http://localhost:8000/ ...');
   });
