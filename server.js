e5d7119c11eb91fa42c86c5e1c78cb35fa5427e2
Iván Alonso <ivan@neverbot.com>
2019-08-30 11:32:23 +0200

    changes in build process, now everything is copied to the build directory

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 
 connect()
-  .use(serveStatic(__dirname))
+  .use(serveStatic(__dirname + '/build'))
   .listen(8000, function() {
     console.log('Server running on http://localhost:8000/ ...');
   });
